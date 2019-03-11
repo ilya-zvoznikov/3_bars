2c42c8045afaf62b7fa47efa9ba441ecfd727a56
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-11 20:19:31 +0700

    reduce nesting

--- bars.py
+++ bars.py
@@ -39,29 +39,27 @@ def get_closest_bar(bars_list, latitude, longitude):
     return closest_bar
 
 
-if __name__ == '__main__':
+if len(sys.argv) > 1:
+    filepath = sys.argv[1]
+else:
+    print('Не указан путь к файлу')
+    exit()
 
-    if len(sys.argv) > 1:
-        filepath = sys.argv[1]
-    else:
-        print('Не указан путь к файлу')
-        exit()
+try:
+    bars_list = load_data(filepath)['features']
+    latitude = float(input('Введите широту Вашего местоположения:\n'))
+    longitude = float(input('Введите долготу Вашего местоположения:\n'))
+except json.decoder.JSONDecodeError:
+    print('Данные не в формате JSON')
+    exit()
+except ValueError:
+    print('Введено некорректное значение')
+    exit()
+except FileNotFoundError:
+    print('Файл не найден')
+    exit()
 
-    try:
-        bars_list = load_data(filepath)['features']
-        latitude = float(input('Введите широту Вашего местоположения:\n'))
-        longitude = float(input('Введите долготу Вашего местоположения:\n'))
-    except json.decoder.JSONDecodeError:
-        print('Данные не в формате JSON')
-        exit()
-    except ValueError:
-        print('Введено некорректное значение')
-        exit()
-    except FileNotFoundError:
-        print('Файл не найден')
-        exit()
-
-    print_bar('Самый большой бар:', get_biggest_bar(bars_list))
-    print_bar('Самый маленький бар:', get_smallest_bar(bars_list))
-    print_bar('Самый близкий бар:',
-              get_closest_bar(bars_list, latitude, longitude))
+print_bar('Самый большой бар:', get_biggest_bar(bars_list))
+print_bar('Самый маленький бар:', get_smallest_bar(bars_list))
+print_bar('Самый близкий бар:',
+          get_closest_bar(bars_list, latitude, longitude))
