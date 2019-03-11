3494f0646b15f0a0109168d176da4c1eb1421070
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-11 17:56:26 +0700

    code refactoring

--- bars.py
+++ bars.py
@@ -5,14 +5,21 @@ from math import sqrt
 
 
 def load_data(filepath):
-    if not os.path.exists(filepath):
-        raise FileNotFoundError
-    with open(filepath, 'r') as file:
-        return json.load(file)
+    try:
+        with open(filepath, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print('Файл не найден')
+        sys.exit()
+    except json.decoder.JSONDecodeError:
+        print('Данные не в формате JSON')
+        sys.exit()
 
 
-def format_out(bar_dict):
-    return bar_dict['properties']['Attributes']['Name']
+def print_bar(message, bar_dict):
+    print('-' * 50)
+    print(message)
+    print(bar_dict['properties']['Attributes']['Name'])
 
 
 def get_biggest_bar(bars_list):
@@ -20,7 +27,7 @@ def get_biggest_bar(bars_list):
         bars_list,
         key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
-    return format_out(biggest_bar)
+    return biggest_bar
 
 
 def get_smallest_bar(bars_list):
@@ -28,40 +35,29 @@ def get_smallest_bar(bars_list):
         bars_list,
         key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
-    return format_out(smallest_bar)
+    return smallest_bar
 
 
-def get_closest_bar(bars_list, longitude, latitude):
+def get_closest_bar(bars_list, latitude, longitude):
     closest_bar = min(
         bars_list,
         key=lambda x: sqrt((latitude - x['geometry']['coordinates'][0]) ** 2 +
                            (longitude - x['geometry']['coordinates'][1]) ** 2))
 
-    return format_out(closest_bar)
+    return closest_bar
 
 
 if __name__ == '__main__':
+    filepath = sys.argv[1] if len(sys.argv) > 1 else ''
+    bars_list = load_data(filepath)['features']
+
     try:
-        filepath = sys.argv[1] if len(sys.argv) > 1 else ''
-        bars_list = load_data(filepath)['features']
-
-        print('Самый большой бар:')
-        print(get_biggest_bar(bars_list))
-        print('-' * 50)
-
-        print('Самый маленький бар:')
-        print(get_smallest_bar(bars_list))
-        print('-' * 50)
-
-        latitude = float(input('Введите широту Вашего месторасположения:\n'))
-        longitude = float(input('Введите долготу Вашего месторасположения:\n'))
-
-        print()
-        print('Самый близкий бар:')
-        print(get_closest_bar(bars_list,
-                              longitude=longitude,
-                              latitude=latitude), '\n')
-    except FileNotFoundError:
-        print('Файл не найден')
+        latitude = float(input('Введите широту Вашего местоположения:\n'))
+        longitude = float(input('Введите долготу Вашего местоположения:\n'))
     except ValueError:
         print('Введено некорректное значение')
+
+    print_bar('Самый большой бар:', get_biggest_bar(bars_list))
+    print_bar('Самый маленький бар:', get_smallest_bar(bars_list))
+    print_bar('Самый близкий бар:',
+              get_closest_bar(bars_list, latitude, longitude))
