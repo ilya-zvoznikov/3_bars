3524b94b4f9ba0dd725ff3fdaa982a8dbda952be
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-14 14:41:25 +0700

    code refactoring

--- bars.py
+++ bars.py
@@ -48,10 +48,12 @@ if __name__ == '__main__':
     else:
         sys.exit('Не указан путь к файлу')
 
-    if not load_data(filepath):
+    loaded_content = load_data(filepath)
+
+    if not loaded_content:
         sys.exit('Файл не найден или данные не в формате JSON')
     else:
-        bars_list = load_data(filepath)['features']
+        bars_list = loaded_content['features']
 
     try:
         latitude = float(input('Введите широту Вашего местоположения:\n'))
@@ -59,7 +61,7 @@ if __name__ == '__main__':
     except ValueError:
         sys.exit('Введено некорректное значение')
 
-    print_bar('Самый большой бар:', get_biggest_bar(bars_list))
-    print_bar('Самый маленький бар:', get_smallest_bar(bars_list))
+    print_bar('Самый большой бар:', get_biggest_bar(loaded_content))
+    print_bar('Самый маленький бар:', get_smallest_bar(loaded_content))
     print_bar('Самый близкий бар:',
-              get_closest_bar(bars_list, latitude, longitude))
+              get_closest_bar(loaded_content, latitude, longitude))
